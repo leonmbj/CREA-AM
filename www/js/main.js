e346109c5ffbd1f744cb20effbcdc630cb2f20f5
leonmbj <leon@leons-MacBook-Pro.local>
2015-06-29 16:51:01 -0700

    2

---
+++ www/js/main.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+
+  $('.menu').click(function() {
+    var navPosition = $(this).attr('data-position');
+    $('nav.' + navPosition + '').addClass('open');
+    $('body').addClass('menu-open');
+    return false;
+  });
+
+/*
+
+        $("#icone").on("swipe",function(){
+            var navPosition = $(this).attr('data-position');
+            $('nav.' + navPosition + '').addClass('open');
+            $('body').addClass('menu-open');
+            return false;
+        });
+*/
+
+
+  //
+  // Use the javascript below when implementing. The code above is for demo purposes only.
+  //
+  /* $('.menu').click(function() {
+     $('nav').addClass('open');
+     $('body').addClass('menu-open');
+     return false;
+   });*/
+    
+    
+
+
+  $(document).click(function() {
+    $('body').removeClass('menu-open');
+    $('nav').removeClass('open');
+  });
+});
+
+
